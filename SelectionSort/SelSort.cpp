c058f95f83851070318624321ee7efcbdd198da1
moda-it <demianchykbogdan@gmail.com>
2025-10-29 23:18:34 +0200

    add input from terminal

--- SelectionSort/SelSort.cpp
+++ SelectionSort/SelSort.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main() {
-	int a[5] = { 64, 25, 12, 22, 11 };
-	int n = 5;
+	int n;
+	cout << "Enter number of elements: \n";
+	cin >> n;
+
+	int a[100];
+
+	cout << "Enter" << n << " elements: \n";
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
 
 	for (int i = n - 1; i > 0; i--) {
 		int maxI = 0;
